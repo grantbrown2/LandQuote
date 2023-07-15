61bfd875704f166ce4b1cb37c1523fb98b0a820c
SkippedFrames <45058659+SkippedFrames@users.noreply.github.com>
2023-07-14 20:05:20 -0500

    Added Quote Schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -48,4 +48,16 @@ UserSchema.pre('save', function(next) {
         });
     });
 
+const quoteSchema = new mongoose.Schema({
+    datetime: String,
+    address: String,
+    name: String,
+    email: String,
+    number: String,
+    notes: String,
+    pictures: [{ filename: String, path: String }],
+});
+    
+
 module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Quote', quoteSchema);
