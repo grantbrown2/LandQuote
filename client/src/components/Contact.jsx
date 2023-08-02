575b7b351d54dc99312c51597e00dafc19ca22f8
grantbrown2 <gbrownzzzz58@gmail.com>
2023-08-02 19:26:16 -0400

    added basic contact via email - frontend

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import '../styles/Contact.css';
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        phoneNum: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]:value
+        }));
+    };
+
+    const handleSubmission = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/send/email', formData)
+            .then(res =>{console.log("Success", res)})
+            .catch((err)=>{ console.error(`Error ${err}`)});
+    }
+
+
+
     return (
         <div className='contact'>
             <h2>Contact Me</h2>
@@ -11,22 +37,22 @@ const Contact = () => {
             <p>
                 You can reach me through the contact form below or by sending me an email directly at <strong>your.email@example.com</strong>. I'll do my best to get back to you as soon as possible.
             </p>
-            <form>
+            <form onSubmit={handleSubmission}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' id='name' name='name' required />
+                    <input type='text' id='name' name='name' value={formData.name} onChange={handleChange} required />
                 </div>
                 <div>
                     <label htmlFor='phoneNum'>Phone Number:</label>
-                    <input type='tel' id='phoneNum' name='phoneNum' required />
+                    <input type='tel' id='phoneNum' name='phoneNum' value={formData.phoneNum} onChange={handleChange} required />
                 </div>
                 <div>
                     <label htmlFor='email'>Email:</label>
-                    <input type='email' id='email' name='email' required />
+                    <input type='email' id='email' name='email' value={formData.email} onChange={handleChange} required />
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea id='message' name='message' rows='5' required></textarea>
+                    <textarea id='message' name='message' rows='5' value={formData.message} onChange={handleChange} required></textarea>
                 </div>
                 <button type='submit'>Send Message</button>
             </form>
